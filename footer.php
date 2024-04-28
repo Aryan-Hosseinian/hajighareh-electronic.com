5f8c61850a8b0af80fd0baac6db12a8c14ee814e
Aryan Hosseinian <aryanhosseinianmail@gmail.com>
2024-04-28 11:03:08 +0330

    a

---
+++ footer.php
@@ -0,0 +1,116 @@
+<footer id="Footer" class="clearfix">
+                <div class="widgets_wrapper center">
+                    <div class="container">
+                        <div class="column one-third">
+                            <aside class="widget_text widget widget_custom_html">
+                                <div class="textwidget custom-html-widget">
+                                    <div class="image_frame image_item no_link scale-with-grid alignnone no_border">
+                                       <a href="https://www.hajighareh-electronic.com"> <div class="image_wrapper"><img class="scale-with-grid" src="content/itservice4/images/itservice4.png" /></div> </a>
+                                    </div>
+                                    <hr class="no_line" style="margin: 0 auto 20px;" />
+                                    <p>
+                                        تبریز ،  <br />
+                                        خیابان امام خمینی، جنب ایستگاه مترو میدان قطب، <br />
+                                        مجتمع تجاری مهدیه، پلاک 15
+                                    </p>
+                                    <hr class="no_line" style="margin: 0 auto 10px;" />
+                                    <a href="#" class="icon_bar icon_bar_facebook icon_bar_small">
+                                        <span class="t"><i class="icon-facebook"></i></span><span class="b"><i class="icon-facebook"></i></span>
+                                    </a>
+                                    <a href="#" class="icon_bar icon_bar_twitter icon_bar_small">
+                                        <span class="t"><i class="icon-twitter"></i></span><span class="b"><i class="icon-twitter"></i></span>
+                                    </a>
+                                    <a href="#" class="icon_bar icon_bar_instagram icon_bar_small">
+                                        <span class="t"><i class="icon-instagram"></i></span><span class="b"><i class="icon-instagram"></i></span>
+                                    </a>
+                                </div>
+                            </aside>
+                        </div>
+                        <div class="column one-third">
+                            <aside class="widget_text widget widget_custom_html">
+                                <div class="textwidget custom-html-widget">
+                                    <h4 class="itservice4-heading">مرکز راهنمایی</h4>
+                                    <hr class="no_line" style="margin: 0 auto 15px;" />
+                                    <h5>با ما تماس بگیرید</h5>
+                                    <p class="big">
+                                        09145910710
+                                    </p>
+                                    <h5>ایمیل</h5>
+                                    <p class="big">
+                                        <a href="#">hajigare@gmail.com</a>
+                                    </p>
+                                </div>
+                            </aside>
+                        </div>
+                        <div class="column one-third">
+                            <aside class="widget_text widget widget_custom_html">
+                                <div class="textwidget custom-html-widget">
+                                    <h4 class="itservice4-heading"> نماد ها</h4>
+                                    <hr class="no_line" style="margin: 0 auto 15px;" />
+
+                                    <form id="newsletterform">
+                                        <span>
+                                            <img src="content/itservice4/images/ts.png" alt="ts">
+                                           <!-- <input type="text" id="name_news" name="name_news" size="40" aria-required="true" aria-invalid="false" placeholder="نام شما" /> -->
+                                        </span>
+                                        <span>
+
+
+                                        
+                                        <script> 
+                                    
+                                    document.write(
+  '<a href="javascript:viewZibalTrust();" title="درگاه بانکی زیبال"><img style="width:30% " src="https://zibal.ir/trust/assets/1.png" border="0" alt="در گاه بانکی زیبال"></a>'
+)
+function viewZibalTrust() {
+  window.open(
+    'https://gateway.zibal.ir/trustMe/' + window.location.hostname,
+    null,
+    'width=450, height=600, scrollbars=no, resizable=no'
+  )
+}
+                                    
+                                    
+                                    </script>
+
+
+
+                                         <!--   <img width="30%" src="content/itservice4/images/1.svg" alt="ts">  -->
+                                         <!--   <input type="email" id="email_news" name="email_news" size="40" aria-required="true" aria-invalid="false" placeholder="ایمیل شما" /> -->
+                                        </span>
+                                       <!-- <img  width="30%" src="content/itservice4/images/2.svg" alt="ts"> -->
+                                     <!--   <input type="button" value="ارسال" id="submit" onClick="return check_values_news();" />  -->
+                                    </form>
+                                </div>
+                            </aside>
+                        </div>
+                    </div>
+                </div>
+                <div class="footer_copy">
+                    <div class="container">
+                        <div class="column one">
+                            <a id="back_to_top" class="footer_button" href="#"><i class="icon-up-open-big"></i></a>
+                            <div class="copyright">© تمام حقوق محفوظ است. طراحی شده توسط <a target="_blank" rel="nofollow" href="https://www.AryanH.com">AryanH</a></div>
+                            <nav id="social-menu" class="menu-footer-container">
+                                <ul id="menu-footer" class="social-menu">
+                                    <li class="current-menu-item page_item current_page_item">
+                                        <a href="/" aria-current="page">صفحه اصلی</a>
+                                    </li>
+                                    <li>
+                                        <a href="#">درباره ما</a>
+                                    </li>
+                                    <li>
+                                        <a href="#">خدمات</a>
+                                    </li>
+                                    <li>
+                                        <a href="#">پرسش و پاسخ</a>
+                                    </li>
+                                    <li>
+                                        <a href="#">تماس</a>
+                                    </li>
+                                </ul>
+                            </nav>
+                        </div>
+                    </div>
+                </div>
+            </footer>
