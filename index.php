75a1551872328c097b222127eebea70b9d4b4e42
Aryan Hosseinian <aryanhosseinianmail@gmail.com>
2024-04-28 10:57:14 +0330

    a

--- index.php
+++ index.php
@@ -31,54 +31,11 @@
         <div id="Wrapper">
             <div id="Header_wrapper">
                 <header id="Header">
-                    <div id="Top_bar" >
-                        <div class="container" >
-                            <div class="column one" >
-                                <div  class="top_bar_right clearfix" >
-                                    <div class="logo" >
-                                        <a id="logo" href="/" title="BeITService 4 - Betheme" data-height="60" data-padding="30">
-                                            <img style="border-radius: 2%;" class="logo-main scale-with-grid" src="content/itservice4/images/logo.png" data-retina="content/itservice4/images/logo.png" data-height="47" />
-                                            <img class="logo-sticky scale-with-grid" src="content/itservice4/images/logo.png" data-retina="content/itservice4/images/logo.png" data-height="47" />
-                                            <img class="logo-mobile scale-with-grid" src="content/itservice4/images/logo.png" data-retina="content/itservice4/images/logo.png" data-height="47" />
-                                            <img class="logo-mobile-sticky scale-with-grid" src="content/itservice4/images/logo.png" data-retina="content/itservice4/images/logo.png" data-height="47" />
-                                        </a>
-                                    </div>
-                                    <div class="menu_wrapper">
-                                        <nav id="menu">
-                                            <ul id="menu-main-menu" class="menu menu-main">
-                                                <li class="current-menu-item page_item current_page_item">
-                                                    <a href="/"><span>صفحه اصلی</span></a>
-                                                </li>
-                                                <li>
-                                                    <a href="#"><span>خدمات</span></a>
-                                                </li>
-                                                <li>
-                                                    <a href="#"><span>فروشگاه</span></a>
-                                                </li>
-                                                <li>
-                                                    <a href="#"><span>مقالات</span></a>
-                                                </li>
-                                                <li>
-                                                    <a href="#"><span>نقشه برد ها</span></a>
-                                                </li>
-                                                <li>
-                                                    <a href="tel:09123456789">
-                                                        <span><span style="padding: 0; color: #ffffff;">درباره ما</span></span>
-                                                    </a>
-                                                </li>
-                                            </ul>
-                                        </nav>
-                                        <a class="responsive-menu-toggle" href="#"><i class="icon-menu-fine"></i></a>
-                                    </div>
-                                </div>
-                                <div class="top_bar_left">
-                                    <div class="top_bar_left_wrapper">
-                                        <a href="tel:+989145910710" class="action_button">09145910710</a>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+
+                
+                <?php include 'head.php'?>
+
+
                     <div class="mfn-main-slider mfn-rev-slider">
                         <p class="rs-p-wp-fix"></p>
                         <rs-module-wrap id="rev_slider_1_1_wrapper" data-source="gallery" style="background: transparent; padding: 0;">
